dc5357d4c8765cbd5597eccda85f309c50448a03
Jonathan Thomas <jonathanthomas83@gmail.com>
2021-10-15 10:59:17 +0100

    Added initial pages for first iteration

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,16 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.post('/', function (req, res) {
+  res.redirect('claimant-details');
+});
+
+router.post('/claimant-details', function (req, res) {
+  res.redirect('dates');
+});
+
+router.post('/dates', function (req, res) {
+  res.redirect('confirmation');
+});
+
 module.exports = router
