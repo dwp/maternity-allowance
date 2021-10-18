6e07aa5c5acbec5ad6ded6e623b6ec5f655dfedc
Jonathan Thomas <jonathanthomas83@gmail.com>
2021-10-18 12:12:19 +0100

    Added alt and changed confirmation to claim summary

--- app/routes.js
+++ app/routes.js
@@ -14,7 +14,7 @@ router.post('/iteration-1/claimant-details', function (req, res) {
 });
 
 router.post('/iteration-1/dates', function (req, res) {
-  res.redirect('/iteration-1/confirmation');
+  res.redirect('/iteration-1/claim-summary');
 });
 
 // Iteration 2
@@ -28,7 +28,22 @@ router.post('/iteration-2/claimant-details', function (req, res) {
 });
 
 router.post('/iteration-2/dates', function (req, res) {
-  res.redirect('/iteration-2/confirmation');
+  res.redirect('/iteration-2/claim-summary');
+});
+
+
+// Alt
+
+router.post('/', function (req, res) {
+  res.redirect('/iteration-alt/claimant-details');
+});
+
+router.post('/iteration-alt/claimant-details', function (req, res) {
+  res.redirect('/iteration-alt/dates');
+});
+
+router.post('/iteration-alt/dates', function (req, res) {
+  res.redirect('/iteration-alt/claim-summary');
 });
 
 module.exports = router
