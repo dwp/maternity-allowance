6a71055957379ca2a433cb34ba24d67829768d83
Jonathan Thomas <jonathanthomas83@gmail.com>
2023-03-06 16:46:01 +0000

    Added iteration 15 - payslip and payments

--- app/routes.js
+++ app/routes.js
@@ -17,6 +17,7 @@ require('./routes/beta-private/iteration-11')(router)
 require('./routes/beta-private/iteration-12')(router)
 require('./routes/beta-private/iteration-13')(router)
 require('./routes/beta-private/iteration-14')(router)
+require('./routes/beta-private/iteration-15')(router)
 
 // releases
 require('./routes/releases/1-0-3')(router)
